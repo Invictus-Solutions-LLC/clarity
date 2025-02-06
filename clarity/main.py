c457583c12f523935094a0318bd8ef88e2b94371
Invictus808 <xinvictus808x@gmail.com>
2025-02-05 23:48:47 -1000

    [update]: download .mp4, use service account instead of oauth2.0 client id

--- clarity/main.py
+++ clarity/main.py
@@ -4,7 +4,7 @@ import json
 
 from dotenv import load_dotenv
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
+from google.oauth2 import service_account
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
@@ -13,63 +13,53 @@ from googleapiclient.http import MediaIoBaseDownload
 
 load_dotenv()
 
-GCLOUD_CLIENT_ID = os.getenv('GCLOUD_CLIENT_ID', '')
-GCLOUD_CLIENT_SECRET = os.getenv('GCLOUD_CLIENT_SECRET', '')
+GCLOUD_TYPE = os.getenv('GCLOUD_TYPE', '')
 GCLOUD_PROJECT_ID = os.getenv('GCLOUD_PROJECT_ID', '')
+GCLOUD_PRIVATE_KEY_ID = os.getenv('GCLOUD_PRIVATE_KEY_ID', '')
+GCLOUD_PRIVATE_KEY = os.getenv('GCLOUD_PRIVATE_KEY', '')
+GCLOUD_CLIENT_EMAIL = os.getenv('GCLOUD_CLIENT_EMAIL', '')
+GCLOUD_CLIENT_ID = os.getenv('GCLOUD_CLIENT_ID', '')
 GCLOUD_AUTH_URI = os.getenv('GCLOUD_AUTH_URI', '')
 GCLOUD_TOKEN_URI = os.getenv('GCLOUD_TOKEN_URI', '')
 GCLOUD_AUTH_PROVIDER_X509_CERT_URL = os.getenv('GCLOUD_AUTH_PROVIDER_X509_CERT_URL', '')
+GCLOUD_CLIENT_X509_CERT_URL = os.getenv('GCLOUD_CLIENT_X509_CERT_URL', '')
+GCLOUD_UNIVERSE_DOMAIN = os.getenv('GCLOUD_UNIVERSE_DOMAIN', '')
+
 GDRIVE_FOLDER_ID = os.getenv('GDRIVE_FOLDER_ID', '')
 
-MIME_TYPE_GSLIDES = 'application/vnd.google-apps.presentation'
-MIME_TYPE_PPTX = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+MIME_TYPES = {
+    'gslides': 'application/vnd.google-apps.presentation',
+    'pptx':'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+    'mp4': 'video/mp4',
+}
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
 CREDENTIALS_JSON = {
-    'installed': {
-        'client_id': GCLOUD_CLIENT_ID,
-        'project_id': GCLOUD_PROJECT_ID,
-        'auth_uri': GCLOUD_AUTH_URI,
-        'token_uri': GCLOUD_TOKEN_URI,
-        'auth_provider_x509_cert_url': GCLOUD_AUTH_PROVIDER_X509_CERT_URL,
-        'client_secret': GCLOUD_CLIENT_SECRET,
-        'redirect_uris': [
-            'http://localhost'
-        ]
-    }
+    'type': GCLOUD_TYPE,
+    'project_id': GCLOUD_PROJECT_ID,
+    'private_key_id': GCLOUD_PRIVATE_KEY_ID,
+    'private_key': GCLOUD_PRIVATE_KEY,
+    'client_email': GCLOUD_CLIENT_EMAIL,
+    'client_id': GCLOUD_CLIENT_ID,
+    'auth_uri': GCLOUD_AUTH_URI,
+    'token_uri': GCLOUD_TOKEN_URI,
+    'auth_provider_x509_cert_url': GCLOUD_AUTH_PROVIDER_X509_CERT_URL,
+    'client_x509_cert_url': GCLOUD_CLIENT_X509_CERT_URL,
+    'universe_domain': GCLOUD_UNIVERSE_DOMAIN,
 }
 
 with open('credentials.json', 'w') as f:
     json.dump(CREDENTIALS_JSON, f, indent=4)
+
 
 def main():
     '''
         Shows basic usage of the Drive v3 API.
         Prints the names and ids of the first 10 files the user has access to.
     '''
-    credentials = None
+    credentials = service_account.Credentials.from_service_account_file('credentials.json', scopes=SCOPES)
 
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        credentials = Credentials.from_authorized_user_file('token.json', SCOPES)
-    
-    # If there are no (valid) credentials available, let the user log in.
-    if not credentials or not credentials.valid:
-        if credentials and credentials.expired and credentials.refresh_token:
-            credentials.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES
-            )
-            credentials = flow.run_local_server(port=0)
-        
-        #Save the credentials for the next run.
-        with open('token.json', 'w') as f:
-            f.write(credentials.to_json())
-    
     try:
         service = build('drive', 'v3', credentials=credentials)
 
@@ -90,14 +80,14 @@ def main():
         GDRIVE_FILE_ID = response['files'][0]['id']
         GDRIVE_FILE_MIME_TYPE = response['files'][0].get('mimeType')
 
-        if GDRIVE_FILE_MIME_TYPE == MIME_TYPE_GSLIDES:
+        if GDRIVE_FILE_MIME_TYPE == MIME_TYPES['gslides']:
             # Export Google Slides as a PowerPoint format (.pptx)
             response = service.files().export_media(
                 fileId=GDRIVE_FILE_ID,
-                mimeType=MIME_TYPE_PPTX,
+                mimeType=MIME_TYPES['pptx'],
             )
-        elif GDRIVE_FILE_MIME_TYPE == MIME_TYPE_PPTX:
-            # Export PowerPoint
+        elif GDRIVE_FILE_MIME_TYPE == MIME_TYPES['pptx'] or GDRIVE_FILE_MIME_TYPE == MIME_TYPES['mp4']:
+            # Export PowerPoint or MP4
             response = service.files().get_media(
                 fileId=GDRIVE_FILE_ID,
             )
@@ -115,8 +105,8 @@ def main():
             status, done = downloader.next_chunk()
             print(f'Download Progress: {int(status.progress() * 100)}%')
 
-        # Save file as a PowerPoint (.pptx) after download is complete
-        with open('bulletin.pptx', 'wb') as f:
+        # Save file as a PowerPoint (.pptx) or MP4 (.mp4) after download is complete
+        with open('bulletin' + ('.mp4' if GDRIVE_FILE_MIME_TYPE == MIME_TYPES['mp4'] else '.pptx'), 'wb') as f:
             f.write(file_stream.getvalue())
 
     except HttpError as error:
